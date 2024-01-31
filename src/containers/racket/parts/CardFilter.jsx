c3c9be26c96e7ed3582c5272e3695103e7cfbf5a
muharsadika <id.muharsadika@gmail.com>
2024-02-01 03:12:49 +0700

    feat: when filter price empty or deleted then all products appear as before

--- src/containers/racket/parts/CardFilter.jsx
+++ src/containers/racket/parts/CardFilter.jsx
@@ -20,35 +20,45 @@ function CardFilter({ onFilter }) {
     onFilter({ minPrice: '', maxPrice: '', name: e.target.value });
   };
 
+  const handlePriceChange = (setPrice, price) => {
+    setPrice(price);
+    if (price === '') {
+      onFilter({ minPrice: '', maxPrice: '', name });
+    }
+  };
+
+
   return (
-    <div className="flex flex-col gap-3" style={{ border: "1px solid red" }}>
+    <div className="flex flex-col gap-20 py-32">
 
-      <h2 className="text-lg font-semibold">FILTER BY:</h2>
+      {/* <p className="text-5xl font-semibold">
+        FILTER BY:
+      </p> */}
 
-      <div className="flex gap-3 justify-between overflow-y-auto" style={{ border: "5px solid pink" }}>
+      <div className="flex gap-3">
         <input
           type="text"
-          placeholder="Search by Product Name"
+          placeholder="Search..."
           value={name}
           onChange={handleNameChange}
-          className="border rounded-md p-2"
+          className="border rounded-md p-2 w-full hover:border-gray-500"
         />
       </div>
 
-      <div className="flex flex-col gap-3" style={{ border: "5px solid blue" }}>
+      <div className="flex flex-col gap-3">
         <input
           type="text"
-          placeholder="Min Price"
+          placeholder="Rp 0"
           value={minPrice}
-          onChange={(e) => setMinPrice(e.target.value)}
-          className="border rounded-md p-2"
+          onChange={(e) => handlePriceChange(setMinPrice, e.target.value)}
+          className="border rounded-md p-2 w-full hover:border-gray-500"
         />
         <input
           type="text"
-          placeholder="Max Price"
+          placeholder="Rp 99.999.999"
           value={maxPrice}
-          onChange={(e) => setMaxPrice(e.target.value)}
-          className="border rounded-md p-2"
+          onChange={(e) => handlePriceChange(setMaxPrice, e.target.value)}
+          className="border rounded-md p-2 w-full hover:border-gray-500"
         />
         <button onClick={handleApplyPriceFilter} className="px-4 py-2 bg-black text-white rounded-md hover:bg-gray-800">
           Apply Price Filter
