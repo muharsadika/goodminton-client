001a70e151be0e9785e0369414ec284d79177e62
muharsadika <id.muharsadika@gmail.com>
2024-02-01 18:48:25 +0700

    feat: field filter by price display Rp 99.999 when input real-time

--- src/containers/racket/parts/CardFilter.jsx
+++ src/containers/racket/parts/CardFilter.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import PropTypes from 'prop-types';
+import { NumericFormat } from 'react-number-format';
 
 function CardFilter({ onFilter }) {
   const [minPrice, setMinPrice] = useState('');
@@ -20,21 +21,8 @@ function CardFilter({ onFilter }) {
     onFilter({ minPrice: '', maxPrice: '', name: e.target.value });
   };
 
-  const handlePriceChange = (setPrice, price) => {
-    setPrice(price);
-    if (price === '') {
-      onFilter({ minPrice: '', maxPrice: '', name });
-    }
-  };
-
-
   return (
     <div className="flex flex-col gap-20 py-32">
-
-      {/* <p className="text-5xl font-semibold">
-        FILTER BY:
-      </p> */}
-
       <div className="flex gap-3">
         <input
           type="text"
@@ -46,18 +34,26 @@ function CardFilter({ onFilter }) {
       </div>
 
       <div className="flex flex-col gap-3">
-        <input
-          type="text"
+        <NumericFormat
+          thousandSeparator={'.'}
+          decimalSeparator={','}
+          prefix={'Rp '}
           placeholder="Rp 0"
-          value={minPrice}
-          onChange={(e) => handlePriceChange(setMinPrice, e.target.value)}
+          onValueChange={(values) => {
+            const { value } = values;
+            setMinPrice(value);
+          }}
           className="border rounded-md p-2 w-full hover:border-gray-500"
         />
-        <input
-          type="text"
+        <NumericFormat
+          thousandSeparator={'.'}
+          decimalSeparator={','}
+          prefix={'Rp '}
           placeholder="Rp 99.999.999"
-          value={maxPrice}
-          onChange={(e) => handlePriceChange(setMaxPrice, e.target.value)}
+          onValueChange={(values) => {
+            const { value } = values;
+            setMaxPrice(value);
+          }}
           className="border rounded-md p-2 w-full hover:border-gray-500"
         />
         <button onClick={handleApplyPriceFilter} className="px-4 py-2 bg-black text-white rounded-md hover:bg-gray-800">
@@ -74,3 +70,89 @@ CardFilter.propTypes = {
 };
 
 export default CardFilter;
+
+
+
+
+// import { useState } from 'react';
+// import PropTypes from 'prop-types';
+
+// function CardFilter({ onFilter }) {
+//   const [minPrice, setMinPrice] = useState('');
+//   const [maxPrice, setMaxPrice] = useState('');
+//   const [name, setName] = useState('');
+//   const [displayMinPrice, setDisplayMinPrice] = useState('');
+//   const [displayMaxPrice, setDisplayMaxPrice] = useState('');
+
+//   const handleApplyPriceFilter = () => {
+//     if ((minPrice !== '' && isNaN(minPrice)) || (maxPrice !== '' && isNaN(maxPrice))) {
+//       alert('Please enter valid prices.');
+//       return;
+//     }
+
+//     onFilter({ minPrice: parseFloat(minPrice), maxPrice: parseFloat(maxPrice), name });
+//   };
+
+//   const handleNameChange = (e) => {
+//     setName(e.target.value);
+//     onFilter({ minPrice: '', maxPrice: '', name: e.target.value });
+//   };
+
+//   const handlePriceChange = (setPrice, setDisplayPrice, price) => {
+//     setPrice(price);
+//     setDisplayPrice(price);
+//     if (price === '') {
+//       onFilter({ minPrice: '', maxPrice: '', name });
+//     }
+//   };
+
+//   const formatCurrency = (value) => {
+//     return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR', minimumFractionDigits: 0 }).format(value);
+//   };
+
+
+//   return (
+//     <div className="flex flex-col gap-20 py-32">
+//       <div className="flex gap-3">
+//         <input
+//           type="text"
+//           placeholder="Search..."
+//           value={name}
+//           onChange={handleNameChange}
+//           className="border rounded-md p-2 w-full hover:border-gray-500"
+//         />
+//       </div>
+
+//       <div className="flex flex-col gap-3">
+//         <input
+//           type="text"
+//           placeholder="Rp 0"
+//           value={displayMinPrice}
+//           onChange={(e) => handlePriceChange(setMinPrice, setDisplayMinPrice, e.target.value.replace(/\D/g, ''))}
+//           onBlur={(e) => setDisplayMinPrice(formatCurrency(e.target.value))}
+//           onFocus={(e) => setDisplayMinPrice(e.target.value.replace(/\D/g, ''))}
+//           className="border rounded-md p-2 w-full hover:border-gray-500"
+//         />
+//         <input
+//           type="text"
+//           placeholder="Rp 99.999.999"
+//           value={displayMaxPrice}
+//           onChange={(e) => handlePriceChange(setMaxPrice, setDisplayMaxPrice, e.target.value.replace(/\D/g, ''))}
+//           onBlur={(e) => setDisplayMaxPrice(formatCurrency(e.target.value))}
+//           onFocus={(e) => setDisplayMaxPrice(e.target.value.replace(/\D/g, ''))}
+//           className="border rounded-md p-2 w-full hover:border-gray-500"
+//         />
+//         <button onClick={handleApplyPriceFilter} className="px-4 py-2 bg-black text-white rounded-md hover:bg-gray-800">
+//           Apply Price Filter
+//         </button>
+//       </div>
+
+//     </div>
+//   );
+// }
+
+// CardFilter.propTypes = {
+//   onFilter: PropTypes.func.isRequired,
+// };
+
+// export default CardFilter;
