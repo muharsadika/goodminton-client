ee0df8da73bad1f6aa5d7d0c66b463adef2089cc
muharsadika <id.muharsadika@gmail.com>
2024-02-07 23:31:50 +0700

    style: test page

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,11 +1,19 @@
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 import useAuth from '../hooks/useAuth'
 
 function Test() {
   const { userData } = useAuth()
 
   return (
-    <div className='text-3xl'>
-      ini userData: {userData}
+    <div>
+      <div>
+        <Navbar />
+      </div>
+      <div className='text-3xl'>
+        ini userData: {userData}
+      </div>
+      <Footer />
     </div>
   )
 }
