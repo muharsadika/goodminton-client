57b76c4e5864b44a3fb303a6d63b3ba48a77a60d
muharsadika <id.muharsadika@gmail.com>
2024-02-10 21:11:51 +0700

    refactor: remove devtools and login fix login page

--- src/containers/login/LoginContainer.jsx
+++ src/containers/login/LoginContainer.jsx
@@ -18,74 +18,72 @@ function LoginContainer() {
             />
           </div>
           {/* Right: Login Form */}
-          <form>
-            <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-              <h1 className="text-2xl font-semibold mb-4">Login</h1>
-              <form action="#" method="POST">
-                {/* Username Input */}
-                <div className="mb-4">
-                  <label htmlFor="username" className="block text-gray-600">
-                    Username
-                  </label>
-                  <input
-                    type="text"
-                    id="username"
-                    name="username"
-                    className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-                    autoComplete="off"
-                    onChange={handleChange}
-                  />
-                </div>
-                {/* Password Input */}
-                <div className="mb-4">
-                  <label htmlFor="password" className="block text-gray-600">
-                    Password
-                  </label>
-                  <input
-                    type="password"
-                    id="password"
-                    name="password"
-                    className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-                    autoComplete="off"
-                    onChange={handleChange}
-                  />
-                </div>
-                {/* Remember Me Checkbox */}
-                <div className="mb-4 flex items-center">
-                  <input
-                    type="checkbox"
-                    id="remember"
-                    name="remember"
-                    className="text-blue-500"
-                  />
-                  <label htmlFor="remember" className="text-gray-600 ml-2">
-                    Remember Me
-                  </label>
-                </div>
-                {/* Forgot Password Link */}
-                <div className="mb-6 text-blue-500">
-                  <a href="#" className="hover:underline">
-                    Forgot Password?
-                  </a>
-                </div>
-                {/* Login Button */}
-                <button
-                  type="submit"
-                  className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
-                  onClick={handleLogin}
-                >
-                  Login
-                </button>
-              </form>
-              {/* Sign up  Link */}
-              <div className="mt-6 text-center">
-                <p>Don’t have an account?</p>
-                <a href="/register" className="hover:underline text-blue-500">
-                  Sign up Here
+          <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+            <h1 className="text-2xl font-semibold mb-4">Login</h1>
+            <form action="#" method="POST">
+              {/* Username Input */}
+              <div className="mb-4">
+                <label htmlFor="username" className="block text-gray-600">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                  autoComplete="off"
+                  onChange={handleChange}
+                />
+              </div>
+              {/* Password Input */}
+              <div className="mb-4">
+                <label htmlFor="password" className="block text-gray-600">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                  autoComplete="off"
+                  onChange={handleChange}
+                />
+              </div>
+              {/* Remember Me Checkbox */}
+              <div className="mb-4 flex items-center">
+                <input
+                  type="checkbox"
+                  id="remember"
+                  name="remember"
+                  className="text-blue-500"
+                />
+                <label htmlFor="remember" className="text-gray-600 ml-2">
+                  Remember Me
+                </label>
+              </div>
+              {/* Forgot Password Link */}
+              <div className="mb-6 text-blue-500">
+                <a href="#" className="hover:underline">
+                  Forgot Password?
                 </a>
               </div>
+              {/* Login Button */}
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+                onClick={handleLogin}
+              >
+                Login
+              </button>
+            </form>
+            {/* Sign up  Link */}
+            <div className="mt-6 text-center">
+              <p>Don’t have an account?</p>
+              <a href="/register" className="hover:underline text-blue-500">
+                Sign up Here
+              </a>
             </div>
-          </form>
+          </div>
         </div>
       </>
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './assets/css/App.css'
 import './assets/css/index.css'
 import { QueryClientProvider, QueryClient } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
+// import { ReactQueryDevtools } from 'react-query/devtools'
 import { Provider } from 'react-redux'
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './stores/rootReducer.jsx'
@@ -20,7 +20,7 @@ const store = configureStore({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={client}>
-      <ReactQueryDevtools initialIsOpen={true} />
+      {/* <ReactQueryDevtools initialIsOpen={true} /> */}
       <Provider store={store}>
         <App />
       </Provider>
