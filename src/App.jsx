817b2f1086262db3742f3ba22faf3b2076676eff
muharsadika <id.muharsadika@gmail.com>
2024-02-19 23:47:16 +0700

    clean: cleaning Main, App, Route

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "./redux/rootReducer";
-// import { useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
-// import { login } from './redux/slice/authSlice';
 
 export default function App() {
   const client = new QueryClient();
@@ -14,16 +11,6 @@ export default function App() {
   const store = configureStore({
     reducer: rootReducer,
   })
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   const token = localStorage.getItem('token');
-  //   if (token) {
-  //     // Anda mungkin perlu mengubah ini tergantung pada bagaimana action login Anda bekerja
-  //     dispatch(login({ token }));
-  //   }
-  // }, [dispatch]);
-
 
   return (
     <QueryClientProvider client={client}>
