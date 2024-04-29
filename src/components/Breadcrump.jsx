7faeb2d78e1da0e32e5087dc6f0aa346015f2a73
muharsadika <id.muharsadika@gmail.com>
2024-04-29 14:26:58 +0700

    refactor: fixing breadcrump set

--- src/components/Breadcrump.jsx
+++ src/components/Breadcrump.jsx
@@ -1,40 +1,40 @@
 import { RxSlash } from "react-icons/rx";
 import { Link } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { useRacket } from "../containers/racket/racketDetail/hook/useRacket";
 
-function Breadcrump({ link, page }) {
-
-  Breadcrump.propTypes = {
-    link: PropTypes.string,
-    page: PropTypes.string
-  }
-
-  const { id } = useParams();
-  const { data: racket } = useRacket(id);
-
+function Breadcrump({ brand, category, name }) {
   return (
     <div className="text-sm">
       <nav className="text-gray-500">
         <ol className="list-none p-0 inline-flex">
-          <li className="flex items-center">
+          <li className="flex items-center capitalize">
             <Link to="/" className="hover:text-slate-700">Home</Link>
             <RxSlash className="w-4 h-4 mx-2" />
           </li>
 
-          <li className="flex items-center">
-            <Link to={link} className="hover:text-slate-700">{page}</Link>
+          <li className="flex items-center capitalize">
+            <Link to="/all-products" className="hover:text-slate-700">{brand}</Link>
+            <RxSlash className="w-4 h-4 mx-2" />
+          </li>
+
+          <li className="flex items-center capitalize">
+            <Link to={`/category/${category}`} className="hover:text-slate-700">{category} </Link>
             <RxSlash className="w-4 h-4 mx-2" />
           </li>
 
           <li className="flex items-center hover:text-slate-700 font-semibold">
-            {racket && <Link to={`/category/racket/${racket.id}`}>{racket.product_name}</Link>}
+            {name}
           </li>
         </ol>
       </nav>
     </div>
   )
+}
+
+Breadcrump.propTypes = {
+  brand: PropTypes.string,
+  category: PropTypes.string,
+  name: PropTypes.string,
 }
 
 export default Breadcrump;
