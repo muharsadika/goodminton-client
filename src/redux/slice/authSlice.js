12264e07ccc82b376946b72e5417069fd2f4dafb
muharsadika <id.muharsadika@gmail.com>
2024-06-10 11:17:27 +0700

    fix: response error message

--- src/redux/slice/authSlice.js
+++ src/redux/slice/authSlice.js
@@ -1,116 +1,112 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { API } from '../../utils/API';
 
-const initialState = {
-  id: '',
-  email: '',
-  fullname: '',
-  status: 'idle',
-  error: null,
-};
-
 export const login = createAsyncThunk(
-  'auth/login',
-  async ({ username, password }) => {
-    const response = await API.post('/buyer/login', {
-      username,
-      password,
-    });
-    // console.log(response.data);
-    return response.data;
-  }
+    'auth/login',
+    async ({ username, password }, thunkAPI) => {
+        try {
+            const response = await API.post('/buyer/login', {
+                username,
+                password,
+            });
+            // console.log('response', response);
+            return response.data;
+        } catch (error) {
+            // console.log('error', error);
+            return thunkAPI.rejectWithValue(error.response.data.message);
+        }
+    }
 );
 
 export const register = createAsyncThunk(
-  'auth/register',
-  async ({ fullname, email, username, password }, thunkAPI) => {
-    try {
-      const response = await API.post('/buyer/register', {
-        fullname,
-        email,
-        username,
-        password,
-      })
-
-      return response.data
-    }catch(error){
-      if(error.response && error.response.data) {
-        return thunkAPI.rejectWithValue(error.response.data)
-      } else {
-        return thunkAPI.rejectWithValue({ error: error.message })
-      }
+    'auth/register',
+    async ({ fullname, email, username, password }, thunkAPI) => {
+        try {
+            const response = await API.post('/buyer/register', {
+                fullname,
+                email,
+                username,
+                password,
+            });
+            // console.log(response.data);
+            return response.data;
+        } catch (error) {
+            // console.log('error', error);
+            return thunkAPI.rejectWithValue(error.response.data.message);
+        }
     }
-    // const response = await API.post('/buyer/register', {
-    //   fullname,
-    //   email,
-    //   username,
-    //   password,
-    // });
-    // // console.log(response.data);
-    // return response.data;
-  }
 );
 
 export const logout = createAsyncThunk('auth/logout', async () => {
-  localStorage.removeItem('token');
-  return {};
+    localStorage.removeItem('token');
+    return {};
 });
 
+const initialState = {
+    id: '',
+    email: '',
+    fullname: '',
+    status: 'idle',
+    error: null,
+};
+
 export const authSlice = createSlice({
-  name: 'auth',
-  initialState: initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(login.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(login.fulfilled, (state, action) => {
-        const { id, email, fullname } = action.payload;
-        // console.log(action.payload);
-        localStorage.setItem('token', action.payload.token);
-        state.status = 'succeeded';
-        state.id = id;
-        state.email = email;
-        state.fullname = fullname;
-        state.error = null;
-      })
-      .addCase(login.rejected, (state, action) => {
-        console.log(action);
-        state.status = 'failed';
-        state.error = action.error.message;
-      });
+    name: 'auth',
+    initialState: initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(login.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(login.fulfilled, (state, action) => {
+                // console.log(action.payload);
+                const { id, email, fullname, token } = action.payload;
+                localStorage.setItem('token', token);
+                state.status = 'succeeded';
+                state.id = id;
+                state.email = email;
+                state.fullname = fullname;
+                state.error = null;
+            })
+            .addCase(login.rejected, (state, action) => {
+                // console.log(`action`, action);
+                // console.log(`action.payload`, action.payload);
+                state.status = 'failed';
+                state.error = action.payload;
+            });
 
-    builder
-      .addCase(register.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(register.fulfilled, (state, action) => {
-        const { id, email, fullname, username } = action.payload;
-        state.status = 'succeeded';
-        state.id = id;
-        state.email = email;
-        state.fullname = fullname;
-        state.username = username;
-        state.error = null;
-      })
+        builder
+            .addCase(register.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(register.fulfilled, (state, action) => {
+                // console.log(action.payload);
+                const { id, email, fullname, username } = action.payload;
+                state.status = 'succeeded';
+                state.id = id;
+                state.email = email;
+                state.fullname = fullname;
+                state.username = username;
+                state.error = null;
+            })
 
-      .addCase(register.rejected, (state, action) => {
-        console.log(action);
-        state.status = 'failed';
-        state.error = action.payload.error;
-      });
+            .addCase(register.rejected, (state, action) => {
+                // console.log(action.payload);
+                state.status = 'failed';
+                state.error = action.payload;
+            });
 
-    builder
-      .addCase(logout.pending, (state) => {
-        state.status = 'loading';
-      })
-      .addCase(logout.fulfilled, (state) => {
-        state.status = 'idle';
-        state.id = '';
-        state.email = '';
-        state.fullname = '';
-        state.error = null;
-      });
-  },
+        builder
+            .addCase(logout.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(logout.fulfilled, (state) => {
+                state.status = 'idle';
+                state.id = '';
+                state.email = '';
+                state.fullname = '';
+                state.error = null;
+            });
+    },
 });
