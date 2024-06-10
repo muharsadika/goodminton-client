bfe807394666912c6eaa55d9186ec918bda540e9
muharsadika <id.muharsadika@gmail.com>
2024-06-10 16:34:06 +0700

    refactor: adjust responsive

--- src/pages/auth/AuthLogin.jsx
+++ src/pages/auth/AuthLogin.jsx
@@ -1,17 +1,19 @@
-import AuthImage from '../../containers/auth/AuthImage'
-import LoginForm from '../../containers/auth/login/LoginForm'
+import AuthImage from '../../containers/auth/AuthImage';
+import LoginForm from '../../containers/auth/login/LoginForm';
 
 function AuthLogin() {
-  return (
-    <div className="bg-gray-100 flex justify-center items-center h-screen">
-      <div className="w-1/2">
-        <AuthImage imageUrl={import.meta.env.VITE_CARD_PLAYER_COLLECTION_6} />
-      </div>
-      <div className="w-1/2">
-        <LoginForm />
-      </div>
-    </div>
-  );
+    return (
+        <div className="bg-gray-100 flex flex-col md:flex-row justify-center items-center">
+            <div className="hidden sm:block w-full md:w-1/2">
+                <AuthImage
+                    imageUrl={import.meta.env.VITE_CARD_PLAYER_COLLECTION_6}
+                />
+            </div>
+            <div className="w-full md:w-1/2">
+                <LoginForm />
+            </div>
+        </div>
+    );
 }
 
 export default AuthLogin;
